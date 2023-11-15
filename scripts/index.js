c24ff0d4b855caa3abdccc36dcc9d75af878f1ee
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-11-15 19:08:28 -0300

    add validation basicals

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,25 @@ import { start, startGame } from "./newGame.js";
 const mode = document.querySelector("#switch");
 const ranque = document.querySelector(".boxRanque");
 const initGame = document.querySelector(".init");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
 const handlers = Array.from(document.querySelectorAll(`[class^='part']`));
 
 mode.addEventListener("click", toggleMode);
 ranque.addEventListener("click", openRanque);
-initGame.addEventListener("click", start);
-handlers.forEach((handler, index) => {
-  handler.addEventListener("click", () => startGame(index));
-});
+
+const validation = () => {
+  let value1 = String(player1.value).split("");
+  let value2 = String(player2.value).split("");
+
+  if (value1.length > 4 || value2.length > 4) {
+    initGame.addEventListener("click", start);
+    handlers.forEach((handler, index) => {
+      handler.addEventListener("click", () => startGame(index));
+    });
+  }
+};
+
+setInterval(() => {
+  validation();
+}, 1010);
