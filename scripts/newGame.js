c4ad4b41cc83c19635d3efe0e3362e4effc972b3
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-12-04 23:41:44 -0300

    Add paradigm procedural in page game

--- scripts/newGame.js
+++ scripts/newGame.js
@@ -1,6 +1,130 @@
 import { printPart, printVictory } from "./components.js";
 
-// DOM Elements
+function sortPlayer() {
+  sortCurrentPlayer = players[Math.floor(Math.random() * 2)];
+  currentPlayer.innerHTML = sortCurrentPlayer;
+}
+
+function startGame() {
+  initialize.style.display = "none";
+  ranque.style.display = "none";
+  game.style.display = "flex";
+  exit.style.display = "block";
+  information.style.display = "flex";
+  players = JSON.parse(localStorage.getItem("@TicTacToe:playersNames"));
+
+  if (!sortCurrentPlayer) {
+    setTimeout(sortPlayer, 0);
+  }
+
+  music.loop = true;
+  music.play();
+  exit.addEventListener("click", exitGame);
+  showTime();
+}
+
+function showTime() {
+  finishTimerInterval = setInterval(() => {
+    const dateNow = new Date();
+    const dateDiff = new Date(dateNow - initialDateTime);
+    const minutes = String(dateDiff.getMinutes()).padStart("2", "0");
+    const seconds = String(dateDiff.getSeconds()).padStart("2", "0");
+
+    timeGame.innerHTML = `${minutes}:${seconds}`;
+  }, 1000);
+}
+
+function endGame() {
+  initialize.style.display = "flex";
+  ranque.style.display = "none";
+  game.style.display = "none";
+  exit.style.display = "none";
+  information.style.display = "none";
+
+  window.location.reload(true);
+}
+
+function exitGame() {
+  let conf = confirm("Tem certeza que deseja desistir?");
+  clearInterval(finishTimerInterval);
+
+  if (conf) {
+    music.pause();
+    endGame();
+  } else {
+    startGame();
+  }
+}
+
+function showParts() {
+  arrayParts.forEach((part, indexY) => printPart(part, players, indexY));
+}
+
+function allElementsInSomeLine() {
+  for (let i = 0; i < 7; i += 3) {
+    if (isVictoryCombination(i, i + 1, i + 2)) return true;
+  }
+  return false;
+}
+
+function allElementsInSomeColumn() {
+  for (let i = 0; i < 3; i++) {
+    if (isVictoryCombination(i, i + 3, i + 6)) return true;
+  }
+  return false;
+}
+
+function allElementsInSomeDiagonal() {
+  return isVictoryCombination(0, 4, 8) || isVictoryCombination(2, 4, 6);
+}
+
+function isVictoryCombination(index1, index2, index3) {
+  return (
+    (arrayParts[index1] == players[0] &&
+      arrayParts[index2] == players[0] &&
+      arrayParts[index3] == players[0]) ||
+    (arrayParts[index1] == players[1] &&
+      arrayParts[index2] == players[1] &&
+      arrayParts[index3] == players[1])
+  );
+}
+
+function arrayIsFilled() {
+  arrayParts.filter((element) => element !== undefined).length ===
+    arrayParts.length;
+}
+
+function checkountVictorys() {
+  const draw = "Empate!";
+  const victory = "Vencedor";
+  let empty = "";
+
+  if (
+    allElementsInSomeLine() ||
+    allElementsInSomeColumn() ||
+    allElementsInSomeDiagonal()
+  ) {
+    printVictory(sortCurrentPlayer, victory);
+  } else if (arrayIsFilled()) {
+    printVictory(empty, draw);
+  }
+}
+
+function currentGame(y) {
+  if (!arrayParts[y]) {
+    arrayParts[y] = sortCurrentPlayer;
+
+    showParts();
+    checkountVictorys();
+
+    sortCurrentPlayer =
+      sortCurrentPlayer == players[0] ? players[1] : players[0];
+    currentPlayer.innerHTML = sortCurrentPlayer;
+  } else {
+    soundError.play();
+  }
+}
+
 const initialize = document.querySelector(".containerInitialize");
 const ranque = document.querySelector(".containerRanque");
 const information = document.querySelector(".boxInformation");
@@ -15,158 +139,8 @@ const arrayParts = new Array(9);
 
 let players;
 let sortCurrentPlayer;
-let hour = 0;
-let minute = 0;
-let second = 0;
-let millisecond = 0;
-let cron;
 
-// -----Start Game-----
-const startGame = () => {
-  initialize.style.display = "none";
-  ranque.style.display = "none";
-  game.style.display = "flex";
-  exit.style.display = "block";
-  information.style.display = "flex";
-  players = JSON.parse(localStorage.getItem("@TicTacToe:playersNames"));
-
-  function sortPlayer() {
-    sortCurrentPlayer = players[Math.floor(Math.random() * 2)];
-    currentPlayer.innerHTML = sortCurrentPlayer;
-  }
-
-  if (!sortCurrentPlayer) {
-    setTimeout(sortPlayer, 0);
-  }
-
-  cron = setInterval(() => showTime(), 10);
-  setInterval(() => music.play(), 100);
-  exit.addEventListener("click", exitGame);
-};
-
-// -----Time-----
-const showTime = () => {
-  function returnData(input) {
-    return input >= 10 ? input : `0${input}`;
-  }
-
-  if ((millisecond += 10) == 1000) {
-    millisecond = 0;
-    second++;
-  }
-  if (second == 60) {
-    second = 0;
-    minute++;
-  }
-  if (minute == 60) {
-    minute = 0;
-    hour++;
-  }
-  timeGame.innerText = `${returnData(hour)}:${returnData(minute)}:${returnData(
-    second
-  )}`;
-};
-
-// -----Exit Game-----
-const exitGame = () => {
-  let conf = confirm("Tem certeza que deseja desistir?");
-
-  clearInterval(cron);
-
-  if (conf) {
-    music.pause();
-    endGame();
-  } else {
-    startGame();
-  }
-};
-
-// -----Game-----
-const currentGame = (y) => {
-  if (!arrayParts[y]) {
-    arrayParts[y] = sortCurrentPlayer;
-
-    showParts();
-    checkountVictorys();
-
-    sortCurrentPlayer =
-      sortCurrentPlayer == players[0] ? players[1] : players[0];
-    currentPlayer.innerHTML = sortCurrentPlayer;
-  } else {
-    playErrorSound();
-  }
-};
-
-const playErrorSound = () => {
-  soundError.play();
-  setTimeout(() => {
-    soundError.pause();
-    soundError.currentTime = 0;
-  }, 1000);
-};
-
-const checkountVictorys = () => {
-  const draw = "Empate!";
-  const victory = "Vencedor";
-  let empty = "";
-
-  if (
-    allElementsInSomeLine() ||
-    allElementsInSomeColumn() ||
-    allElementsInSomeDiagonal()
-  ) {
-    printVictory(sortCurrentPlayer, victory);
-  } else if (arrayIsFilled()) {
-    printVictory(empty, draw);
-  }
-};
-
-const arrayIsFilled = () =>
-  arrayParts.filter((element) => element !== undefined).length ===
-  arrayParts.length;
-
-const allElementsInSomeLine = () => {
-  for (let i = 0; i < 7; i += 3) {
-    if (isVictoryCombination(i, i + 1, i + 2)) return true;
-  }
-  return false;
-};
-
-const allElementsInSomeColumn = () => {
-  for (let i = 0; i < 3; i++) {
-    if (isVictoryCombination(i, i + 3, i + 6)) return true;
-  }
-  return false;
-};
-
-const allElementsInSomeDiagonal = () => {
-  return isVictoryCombination(0, 4, 8) || isVictoryCombination(2, 4, 6);
-};
-
-const isVictoryCombination = (index1, index2, index3) => {
-  return (
-    (arrayParts[index1] == players[0] &&
-      arrayParts[index2] == players[0] &&
-      arrayParts[index3] == players[0]) ||
-    (arrayParts[index1] == players[1] &&
-      arrayParts[index2] == players[1] &&
-      arrayParts[index3] == players[1])
-  );
-};
-
-const showParts = () => {
-  arrayParts.forEach((part, indexY) => printPart(part, players, indexY));
-};
-
-// -----End Game-----
-const endGame = () => {
-  initialize.style.display = "flex";
-  ranque.style.display = "none";
-  game.style.display = "none";
-  exit.style.display = "none";
-  information.style.display = "none";
-
-  window.location.reload(true);
-};
+const initialDateTime = new Date();
+let finishTimerInterval;
 
 export const gameFunctions = { startGame, currentGame, endGame, showParts };
