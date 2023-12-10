26ca9212e7859a7d0108537781bf1e8033a47c87
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2023-12-09 22:32:44 -0300

    add function show rank

--- scripts/ranque.js
+++ scripts/ranque.js
@@ -6,6 +6,32 @@ function closeRanque() {
   exitRanque.classList.add("exit");
 }
 
+function createBodyTableRank() {
+  const storageRank = JSON.parse(localStorage.getItem("@tictactoe:rank"));
+  let rankSorted;
+
+  if (storageRank) {
+    rankSorted = storageRank
+      .sort((a, b) => {
+        if (a.victory < b.victory) return 1;
+        if (a.victory > b.victory) return -1;
+        return 0;
+      })
+      .filter((value, index) => index < 10);
+  }
+
+  ranqueValues.innerHTML = "";
+  for (let i = 0; i < 10; i++) {
+    ranqueValues.innerHTML += `
+    <tr>
+      <td>${i + 1}</td>
+      <td>${rankSorted ? rankSorted[i]?.name || "" : ""}</td>
+      <td>${rankSorted ? rankSorted[i]?.victory || "" : ""}</td>
+    </tr>
+    `;
+  }
+}
+
 function openRanque() {
   initialize.style.display = "none";
   ranque.style.display = "block";
@@ -13,11 +39,13 @@ function openRanque() {
   exitRanque.classList.remove("exit");
   exitRanque.classList.add("exitRanque");
 
+  createBodyTableRank();
   exitRanque.addEventListener("click", closeRanque);
 }
 
 const initialize = document.querySelector(".containerInitialize");
 const ranque = document.querySelector(".containerRanque");
+const ranqueValues = document.querySelector(".tableValues tbody");
 const exitRanque = document.querySelector(".exit");
 
 export const rank = { openRanque, closeRanque };
